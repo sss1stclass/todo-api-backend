985a870b7dd51abf951779afeba85c3b08c5e7df
sss1stclass <sohilkr88@gmail.com>
2025-05-18 22:54:54 +0530

    added cors package for cors conflicts due to localhost api

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./database');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
